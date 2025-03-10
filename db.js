2bd0274baf90c44243311cebb6b4fd95109f8c94
Javier Barrera <29jbarrera@gmail.com>
2025-03-10 09:11:48 +0100

    Conexion con postgreSQL

---
+++ db.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const isProduction = process.env.NODE_ENV === 'development'; //Cambiar a production y añadir url a env (desplegado)
+
+const pool = new Pool({
+    connectionString: isProduction
+      ? process.env.DATABASE_URL 
+    : `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`, 
+    ssl: isProduction ? { rejectUnauthorized: false } : false, 
+  });
+
+pool.connect()
+.then(() => console.log(`📦 Conectado a PostgreSQL (${isProduction ? 'Producción' : 'Desarrollo'})`))
+  .catch(err => console.error('❌ Error de conexión', err));
+
+module.exports = pool;
--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('Hola, mundo desde Node.js!');
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/test', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json({ message: 'Conexión exitosa', time: result.rows[0] });
+  } catch (error) {
+    console.error('❌ Error ejecutando la consulta', error);
+    res.status(500).send('Error en la base de datos');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+});
