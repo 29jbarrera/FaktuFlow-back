0f9413d056f51619a1f9b6544a133848e5a0db88
Javier Barrera <29jbarrera@gmail.com>
2025-04-24 12:32:56 +0200

    add request changePassword

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
-const { register, login } = require("../controllers/authController");
+const { verifyToken } = require("../middlewares/authMiddleware");
+const {
+  register,
+  login,
+  changePassword,
+} = require("../controllers/authController");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
@@ -37,6 +42,27 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
     login(req, res);
+  }
+);
+
+router.post(
+  "/change-password",
+  verifyToken,
+  [
+    body("currentPassword")
+      .notEmpty()
+      .withMessage("La contraseña actual es obligatoria"),
+    body("newPassword")
+      .isLength({ min: 4 })
+      .withMessage("La nueva contraseña debe tener al menos 4 caracteres"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    changePassword(req, res);
   }
 );
 
